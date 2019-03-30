40b71b3437071e1ae4c4ef943a7d9cff58ffbeb4
ccb <ccb412727@qq.com>
2019-03-31 06:56:45 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Hello VuePress",
+  description: "Just playing around",
+  // theme: "vuepress-theme-ktquez",
+  base: "/blog/",
+  plugins: [
+    "@vuepress/blog",
+    "@vuepress/back-to-top",
+    "@vuepress/pwa",
+    "@vuepress/pagination"
+  ]
+};
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+categoryIndexPageUrl /category/
+postsDir \_posts
+tagIndexPageUrl /tag/
+permalink /:year/:month/:day/:slug
+
+- 我谁也没有等 谁也不会来
